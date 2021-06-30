cde2f489a496f71f08379c1cdff5dd1ef09c448c
mohammedpythons <mohammedshokai99@gmail.com>
2021-06-30 17:36:48 -0400

    adjust the function and returns true

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
 const wordSearch = (letters, word) => { 
+    let status = true;
     const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalJoin = letters[0].map((column, index) => 
+        letters.map((row) => row[index])
+    );
+
+    const flip = verticalJoin.map((ls) => ls.join(""))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        console.log("new iteration",status, l, word)
+        if (l.includes(word)) {
+          
+            
+            status = true
+            return status
+           
+           
+         } else {
+            
+             status = false;
+         }
+         
     }
-}
+  
+    return status;
+};
+
+
 
 module.exports = wordSearch
